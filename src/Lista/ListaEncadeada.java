08f372d3a9e07a68779e9e60f83f18724bd4f2d4
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2023-09-20 13:40:32 -0300

    [add]: percorrendo a lista

--- src/Lista/ListaEncadeada.java
+++ src/Lista/ListaEncadeada.java
@@ -23,8 +23,26 @@ public class ListaEncadeada<T> {
 
     @Override
     public String toString() {
-        return "ListaEncadeada{" +
-                "inicio=" + inicio +
-                '}';
+        // [1, 2, 3, 4]
+
+        if(this.tamanho == 0) {
+            return "[]";
+        }
+
+        StringBuilder builder = new StringBuilder("[");
+
+        No<T> atual = this.inicio;
+        for(int i=0; i<this.tamanho-1; i++) {
+            builder.append(atual.getElemento()).append(", ");
+            atual = atual.getProximo();
+        }
+        builder.append(atual.getElemento()).append("]");
+//        builder.append(atual.getElemento()).append(", ");
+//        while(atual.getProximo() != null) {
+//            atual = atual.getProximo();
+//            builder.append(atual.getElemento()).append(", ");
+//        }
+
+        return builder.toString();
     }
 }
