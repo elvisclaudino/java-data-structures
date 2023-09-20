a61c322136ba5826446b33c9334e62720e1e0e48
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2023-09-20 11:16:17 -0300

    [update]: aula10

---
+++ src/Fila/Fila.java
@@ -0,0 +1,13 @@
+package Fila;
+
+import Base.EstruturaEstatica;
+
+public class Fila<T> extends EstruturaEstatica<T> {
+    public Fila() {
+        super();
+    }
+
+    public Fila(int capacidade) {
+        super(capacidade);
+    }
+}
---
+++ src/Fila/teste/Aula19.java
@@ -0,0 +1,13 @@
+package Fila.teste;
+
+import Fila.Fila;
+import Pilha.Pilha;
+
+public class Aula19 {
+    public static void main(String[] args) {
+        Fila<Integer> fila = new Fila<Integer>();
+
+        System.out.println(fila.estaVazia()); // true
+        System.out.println(fila.tamanho()); // 0
+    }
+}
