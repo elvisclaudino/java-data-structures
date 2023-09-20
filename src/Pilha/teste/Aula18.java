ed7e2bec383c93d9c026aaa106f29f29cc666c9d
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2023-09-20 10:12:26 -0300

    [update]: aula18

---
+++ src/Pilha/teste/Aula18.java
@@ -0,0 +1,28 @@
+package Pilha.teste;
+
+import Pilha.Pilha;
+
+import java.util.Stack;
+
+public class Aula18 {
+    public static void main(String[] args) {
+        Pilha<Integer> pilha = new Pilha<Integer>();
+        Stack<Integer> stack = new Stack<Integer>();
+
+        System.out.println(stack.isEmpty());
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        System.out.println(stack.isEmpty());
+        System.out.println(stack.size());
+        System.out.println(stack);
+
+        System.out.println(stack.peek()); // Topo da pilha
+
+        System.out.println(stack.pop());
+
+        System.out.println(stack);
+    }
+}
